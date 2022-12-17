49ea22dd6003877f6bfab891a73064088f386061
vjhjv111 <vjhjv111@naver.com>
2022-12-17 19:32:58 +0900

    [커밋 제목] Apply Changes on Main Page [수정 타입] 신규 코드 추가 [수정 파트] 프론트 [수정 기능] 로고 이미지 추가, 헤더 부분 수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
+import tosstechlogo from './img/tosstech.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <img src={tosstechlogo} className="toss-logo" alt="tosstechlogo" />
+        <div className='header-right-elements'>
+          <a className='header-right-just-text' href='https://toss.tech/design'>디자인</a>
+          <a className='header-right-just-text'href='https://toss.tech/tech'>개발</a>          
+          <button className='header-right-button'>채용 바로가기</button>   
+        </div>
+
+        
+      
+      </header>
+      <div>
+      
+        Test Dev
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://localhost:3000/"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
-      </header>
+      </div>
     </div>
   );
 }
